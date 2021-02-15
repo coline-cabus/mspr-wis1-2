9710f8ec0d75c2cf57ae77969f8436d5778dce06
Cabus <coline.cabuspro@gmail.com>
2021-02-15 15:24:29 +0100

    finish-security

--- security.php
+++ security.php
@@ -9,26 +9,6 @@ require_once 'template-parts/header.php'; ?>
 
     <main id="main">
 
-        <section id="main-section">
-            <div class="container">
-                <div class="img-wrapper">
-                    <img src="assets/svg/security.svg" alt="security-icon">
-                </div>
-            </div>
-
-            <div class="container">
-                <div class="text-wrapper">
-                    <h1 class="text-center">Lorem ipsum dolor sit.</h1>
-                    <p class="text-break">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ea magnam placeat quia soluta? A
-                        aliquam autem cum eligendi ipsam nisi perspiciatis similique voluptates. At corporis cupiditate
-                        dignissimos impedit maxime nesciunt tempora veritatis voluptates. Aliquam architecto delectus
-                        dicta
-                        dolorum eaque esse excepturi in libero, magni mollitia nisi quidem sit voluptatibus?</p>
-                </div>
-            </div>
-        </section>
-
-
         <section class="timeline">
 
 
@@ -144,16 +124,10 @@ require_once 'template-parts/header.php'; ?>
                             omnis dolor iste fugit totam quasi inventore!
 
                         </blockquote>
-
                     </div>
-
                 </div>
-
-
             </div>
         </section>
-
-
     </main>
 
 
