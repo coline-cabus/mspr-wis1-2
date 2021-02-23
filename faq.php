3dfc84cb7bb39cfeb56467ab2199ad7d796e9bcd
Dorian Candy <cdy.dorian@gmail.com>
2021-02-23 20:48:17 +0100

    new FAQ

---
+++ faq.php
@@ -0,0 +1,17 @@
+<?php
+
+$page = [
+    'title' => 'Foire aux questions',
+    'description' => 'La description de la page',
+];
+
+require_once 'template-parts/header.php'; ?>
+
+<main>
+    <section>
+
+    </section>
+</main>
+
+
+<?php require_once 'template-parts/footer.php'; ?>
